511d5acfe3b62590c4162d259a2b09954b578a0a
ical <123912919+icaal23@users.noreply.github.com>
2023-10-09 22:51:05 +0800

    Hari Pertama
    Menampilkan kata Hello Word dan Nama

---
+++ H1_CODING.java
@@ -0,0 +1,8 @@
+public class H1_CODING {
+    public static void main(String[] args) {
+        String kata = "Hello Word";
+        String nama = "Muhammad Faisal";
+        System.out.println(kata);
+        System.out.println(nama);
+    }
+}
