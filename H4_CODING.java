77ad47a4793a7f0ba229196cf20a412f1cf1b423
ical <123912919+icaal23@users.noreply.github.com>
2023-10-12 21:14:55 +0800

    Create H4_CODING.java
    Menggunakan Scanner

---
+++ H4_CODING.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class H4_CODING {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan Nama : ");
+        String nama = input.next();
+        System.out.println("Nama saya adalah "+nama);
+    }
+}
