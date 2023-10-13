a6a693008b1f5b692466d1649b3b035b1a47d1da
ical <123912919+icaal23@users.noreply.github.com>
2023-10-13 21:15:34 +0800

    Create H5_CODING.java

---
+++ H5_CODING.java
@@ -0,0 +1,7 @@
+public class H5_CODING {
+    public static void main(String[] args) {
+        String nama1 = "Muhammad ";
+        String nama2 = "Faisal";
+        System.out.println(nama1+nama2);
+    }
+}
