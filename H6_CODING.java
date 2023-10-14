55355f1331c9effae509d97b94cf31beb9a73328
ical <123912919+icaal23@users.noreply.github.com>
2023-10-14 21:23:03 +0800

    Create H6_CODING.java
    Menghitung dan memunculkan True dan False

---
+++ H6_CODING.java
@@ -0,0 +1,11 @@
+public class H6_CODING {
+    public static void main(String[] args) {
+        System.out.println(5+5);
+        System.out.println(5-5);
+        System.out.println(5*5);
+        System.out.println(5/5);
+        System.out.println(5%5);
+        System.out.println(10>5);
+        System.out.println(10<5);
+    }
+}
