9220a9bd36bd289df0018389c7d1fb433b123583
ical <123912919+icaal23@users.noreply.github.com>
2023-10-16 13:03:29 +0800

    Create H8_CODING.java

---
+++ H8_CODING.java
@@ -0,0 +1,17 @@
+package Ngoding;
+import java.util.Scanner;
+public class Coding 100 Hari ISC {
+   public static void main(Strong[] args){
+       Scanner input = new Scanner(System.in);
+       System.out.println("Masukkan Username : ");
+       String username = input.next();
+       System.out.println("Masukkan Password : ");
+       int password = input.nextInt();
+       
+       if("Admin".equals(usename) && password == 1234){
+           System.out.println("Login Berhasil")
+       }else{
+           System.out.println("Login Gagal")
+       }
+   }
+}
