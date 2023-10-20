f4f7e78910f7e5444eb00816783139be29c92ec7
ical <123912919+icaal23@users.noreply.github.com>
2023-10-21 07:22:19 +0800

    Create H13_CODING.java
    Menampilkan huruf kecil dan huruf besar dengan menggunakan method toLowerCase dan toUpperCase

---
+++ H13_CODING.java
@@ -0,0 +1,10 @@
+public class H13_CODING {
+    public static void main(String[] args) {
+        String kata = "Pemrograman Java";
+        String huruf_kecil = kata.toLowerCase();
+        String huruf_besar = kata.toUpperCase();
+        System.out.println("Kata awal = " + kata);
+        System.out.println("Method toLowerCase() = " + huruf_kecil);
+        System.out.println("Method toUpperCase() = " + huruf_besar);
+    }
+}
