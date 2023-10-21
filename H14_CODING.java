c4075f1bcff2d310f5580701bdce0bbe09f5128b
ical <123912919+icaal23@users.noreply.github.com>
2023-10-22 06:38:06 +0800

    Create H14_CODING.java
    Mencari/mendapatkan posisi substring

---
+++ H14_CODING.java
@@ -0,0 +1,9 @@
+public class H14_CODING {
+    public static void main(String[] args) {
+        String kata = "Pemrograman Java";
+        int posisi1 = kata.indexOf ('r');
+        int posisi2 = kata.indexOf ('v');
+        System.out.println("Letak huruf r = " + posisi1);
+        System.out.println("Letak huruf v = " + posisi2);
+    }
+}
