2ac607555f333a6af335bd6ce4cd4771864fed19
ical <123912919+icaal23@users.noreply.github.com>
2023-10-23 11:32:57 +0800

    Create H15_CODING.java
    Mengambil karakter dari tipe data String

---
+++ H15_CODING.java
@@ -0,0 +1,11 @@
+public class H15_CODING {
+    public static void main(String[] args){
+        String kata = "Objek";
+        char a = kata.charAt (0);
+        char b = kata.charAt (2);
+        char c = kata.charAt (4);
+        System.out.println("Index ke-0 adalah " + a);
+        System.out.println("Index ke-2 adalah " + b);
+        System.out.println("Index ke-4 adalah " + c);
+    }
+}
