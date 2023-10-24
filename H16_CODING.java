71e6a53d04b25fd76b936f0edf4cb54a4cf4ac51
ical <123912919+icaal23@users.noreply.github.com>
2023-10-24 20:56:55 +0800

    Create H16_CODING.java
    Mengambil kata dengan menggunakan substring

---
+++ H16_CODING.java
@@ -0,0 +1,9 @@
+public class H16_CODING {
+    public static void main(String[] args){
+       String kata = "Aku sedang belajar ngoding bahasa java dan mengikuti ISC";
+       String subkata = kata.substring(0,38);
+       String subkata2 = kata.substring(39);
+        System.out.println("Substring kata (1): " + subkata);
+        System.out.println("Substring kata (2): " + subkata2);
+    }
+}
