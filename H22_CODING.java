c656001c9e1ad71798416be7f718ed5c65fada6f
ical <123912919+icaal23@users.noreply.github.com>
2023-10-30 21:50:07 +0800

    Create H22_CODING.java
    Belajar penggunaan Array

---
+++ H22_CODING.java
@@ -0,0 +1,9 @@
+public class H22_CODING {
+    public static void main(String[] args) {
+        // Ical [0], Faisal [1]
+        String nama[] = {"Ical","Faisal"};
+        for (int i = 0; i < nama.length; i++){
+            System.out.println(nama[i]);
+        }
+    }
+}
