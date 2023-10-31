7f8f97a307d9081e129b1a7fd1cc9f10c4b94e40
ical <123912919+icaal23@users.noreply.github.com>
2023-11-01 02:23:18 +0800

    Create H23_CODING.java
    Menampilkan nama secara acak menggunakan array

---
+++ H23_CODING.java
@@ -0,0 +1,9 @@
+public class H23_CODING {
+    public static void main(String[] args) {
+        //Faisal [0], Ical [1], Sal[2]
+        String nama[] = {"Faisal", "Ical" , "Sal"};
+        System.out.println(nama[0]);
+        System.out.println(nama[2]);
+        System.out.println(nama[1]);
+    }
+}
