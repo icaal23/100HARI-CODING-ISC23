9575799f7304ba4a575ec05e5637dc48bb9e7892
ical <123912919+icaal23@users.noreply.github.com>
2023-11-01 20:26:57 +0800

    Create H24_CODING.java
    Penggunaan Class Arrays untuk melakukan sorting/pengurutan

---
+++ H24_CODING.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class H24_CODING {
+    public static void main(String[] args) {
+        int[] angka = {3, 2, 4, 1, 5};
+        Arrays.sort(angka);
+        System.out.println(Arrays.toString(angka));
+    }
+}
