ea8915b7eee4238a57142b995ea08c51916861a4
ical <123912919+icaal23@users.noreply.github.com>
2023-11-02 19:49:59 +0800

    Create H25_CODING.java
    Belajar penggunaan Ternary Operator, menampilkan nilai maxsimum

---
+++ H25_CODING.java
@@ -0,0 +1,9 @@
+public class H25_CODING {
+    public static void main(String[] args) {
+        int nomor1 = 50;
+        int nomor2 = 60;
+        int max = (nomor1 > nomor2) ? nomor1 : nomor2;
+        
+        System.out.println("Nilai Max : " + max);
+    }
+}
