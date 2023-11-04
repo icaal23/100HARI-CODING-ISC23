d635328b06517dae2205d0a67996a29657e42d7b
ical <123912919+icaal23@users.noreply.github.com>
2023-11-04 22:15:47 +0800

    Create H27_CODING.java
    Belajar menggunakan perulangan for

---
+++ H27_CODING.java
@@ -0,0 +1,17 @@
+public class H27_CODING {
+    public static void main(String[] args) {
+        
+        System.out.println("============");
+        System.out.println("ANGKA 1-5");
+        System.out.println("============");
+        for(int i = 1; i <= 5; i++){
+            System.out.println("Angka ke-" + i);
+        }
+        System.out.println("============");
+        System.out.println("ANGKA 10-1");
+        System.out.println("============");
+        for(int a = 10; a >= 1; a--){
+            System.out.println("angka ke-" + a);
+        }
+    }
+}
