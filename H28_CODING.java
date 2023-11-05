af96743059b9f65de93cc84a4b0897a582154db4
ical <123912919+icaal23@users.noreply.github.com>
2023-11-05 08:10:01 +0800

    Create H28_CODING.java
    Belajar menggunakan For Each

---
+++ H28_CODING.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class H28_CODING {
+    public static void main(String[] args) {
+        List<Integer> angka = new ArrayList<>();
+        
+        angka.add(1);
+        angka.add(2);
+        angka.add(3);
+        angka.add(4);
+        angka.add(5);
+        
+        for(Integer number : angka){
+            System.out.println(number);
+        }
+    }
+}
