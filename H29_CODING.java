5c0e817836c44f59111693c82358b98ec569755c
ical <123912919+icaal23@users.noreply.github.com>
2023-11-07 01:16:00 +0800

    Create H29_CODING.java
    Disini saya menggunakan ArrayList, setelah itu saya menambahkan item ke ArrayList, dan menampilkan item ArrayList

---
+++ H29_CODING.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class H29_CODING {
+    public static void main(String[] args) {
+        ArrayList<String> daftarBarang = new ArrayList<>();
+        daftarBarang.add("[1] Laptop");
+        daftarBarang.add("[2] PC");
+        daftarBarang.add("[3] Monitor");
+        
+        System.out.println("Daftar Barang : ");
+        for (String barang : daftarBarang){
+            System.out.println(barang);
+        }
+        
+    }
+}
