20c42f6678ac49ac3e01ca90651390a63f973a64
ical <123912919+icaal23@users.noreply.github.com>
2023-11-07 21:24:23 +0800

    Create H30_CODING.java
    Disini saya belajar index
    Pertama saya membuat Array Intege
    Kedua saya mengakses elemen Array dengan indeks
    dan menampilkan output

---
+++ H30_CODING.java
@@ -0,0 +1,13 @@
+public class H30_CODING {
+    public static void main(String[] args) {
+        int[] angka = {10,20,30,40,50};
+        
+        int indexPertama = 0;
+        int indexKedua = 2;
+        int indexKetiga = 4;
+        
+        System.out.println("Index Pertama : " + angka[indexPertama]);
+        System.out.println("Index Kedua : " + angka[indexKedua]);
+        System.out.println("Index Ketiga : " + angka[indexKetiga]);
+    }
+}
