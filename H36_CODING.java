a2e889c5ca7378229552b64534b1ef3cf271e114
ical <123912919+icaal23@users.noreply.github.com>
2023-11-14 00:18:33 +0800

    Create H36_CODING.java
    Membuat program mengfungsikan static

---
+++ H36_CODING.java
@@ -0,0 +1,9 @@
+public class H36_CODING {
+    public static void FungsiStatic(){
+        System.out.println("Ini adalah fungsi static");
+    }
+    
+    public static void main(String[] args) {
+        H36_CODING.FungsiStatic();
+    }
+}
