0f4b6c2641161a85c2722691da0ef0d5e6bef009
ical <123912919+icaal23@users.noreply.github.com>
2023-11-14 22:11:42 +0800

    Create H37_CODING.java
    Program fungsi void

---
+++ H37_CODING.java
@@ -0,0 +1,14 @@
+public class H37_CODING {
+    public static void printHello(){
+        System.out.println("Hello Word");
+    }
+    
+    public static void printMessage(String pesan){
+        System.out.println(pesan);
+    }
+    
+    public static void main(String [] args){
+        printHello();
+        printMessage("Selamat Datang");
+    }
+}
