8c15d78ab5282c9601c8b8018989e1dc329fd8cd
ical <123912919+icaal23@users.noreply.github.com>
2023-11-15 22:00:50 +0800

    Create H38_CODING.java
    Membuat program Fungsi return value (non-void)

---
+++ H38_CODING.java
@@ -0,0 +1,13 @@
+public class H38_CODING {
+    public static void main(String[] args) {
+        int hasilPenjumlahan = tambah(10, 5);
+
+        System.out.println("Hasil penjumlahan: " + hasilPenjumlahan);
+    }
+
+    public static int tambah(int a, int b) {
+        int hasil = a + b;
+
+        return hasil;
+    }
+}
