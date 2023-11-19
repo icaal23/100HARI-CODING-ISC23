d1796b70c9345c523639ff41bda11268169e684f
ical <123912919+icaal23@users.noreply.github.com>
2023-11-19 20:23:37 +0800

    Create H42_CODING.java
    Membuat program charAt

---
+++ H42_CODING.java
@@ -0,0 +1,14 @@
+public class H42_CODING {
+    public static void main(String[] args) {
+        // Mendefinisikan sebuah string
+        String str = "Muhammad Faisal";
+
+        // Menggunakan charAt() untuk mendapatkan karakter pada indeks tertentu
+        char character1 = str.charAt(0); // Karakter pertama (indeks 0)
+        char character7 = str.charAt(7); // Karakter ketujuh (indeks 7)
+
+        // Menampilkan karakter yang diambil
+        System.out.println("Karakter pertama: " + character1);
+        System.out.println("Karakter ketujuh: " + character7);
+    }
+}
