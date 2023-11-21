93cde1ff95bb63c3c634ab158b8f0e828cbbfd21
ical <123912919+icaal23@users.noreply.github.com>
2023-11-21 20:37:48 +0800

    Create H44_CODING.java
    Program penggunaan Constant

---
+++ H44_CODING.java
@@ -0,0 +1,11 @@
+public class H44_CODING {
+    // Deklarasi Constant menggunakan final
+    public static final int ANGKA = 10;
+    public static final String KATA = "Teknik Informatika";
+
+    public static void main(String[] args) {
+        // Penggunaan Constant
+        System.out.println("Nilai konstanta angka : " + ANGKA);
+        System.out.println("Nilai konstanta kata : " + KATA);
+    }
+}
