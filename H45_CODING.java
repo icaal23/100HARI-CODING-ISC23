9b9917842a2ee2edad55f15850c2ad960c436197
ical <123912919+icaal23@users.noreply.github.com>
2023-11-22 23:16:03 +0800

    Create H45_CODING.java
    Membuat program menggunakan Contains

---
+++ H45_CODING.java
@@ -0,0 +1,10 @@
+public class H45_CODING {
+    public static void main(String[] args) {
+        String kata = "Hello Word";
+        boolean kata1 = kata.contains("Hello");
+        boolean kata2 = kata.contains("Informatika");
+        
+        System.out.println("Apakah ada kata 'Hello' ?" + kata1);
+        System.out.println("Apakah ada kata 'Informatika' ? " + kata2);
+    }
+}
