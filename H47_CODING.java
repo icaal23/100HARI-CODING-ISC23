4efae31792ad8b0019060ec7888e6f7f0c0e4c59
ical <123912919+icaal23@users.noreply.github.com>
2023-11-24 21:39:30 +0800

    Create H47_CODING.java
    Program menggunakan Concatenation

---
+++ H47_CODING.java
@@ -0,0 +1,11 @@
+public class H47_CODING {
+    public static void main(String[] args) {
+        String namaAwal = "Muhammad";
+        String namaTerakhir = "Faisal";
+
+        // Menggunakan operator concatenation (+) untuk menggabungkan string
+        String namaLengkap = namaAwal + " " + namaTerakhir;
+        // Menampilkan hasil concatenation
+        System.out.println("Nama Lengkap : " + namaLengkap);
+    }
+}
