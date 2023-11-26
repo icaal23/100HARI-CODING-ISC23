8ac98b617a3bdb5d8af4ce1403295c7a5d66ad87
ical <123912919+icaal23@users.noreply.github.com>
2023-11-26 20:11:18 +0800

    Create H49_CODING.java
    Program menggunakan equalsIgnoreCase

---
+++ H49_CODING.java
@@ -0,0 +1,12 @@
+public class H49_CODING {
+    public static void main(String[] args) {
+        String kata1 = "Halo";
+        String kata2 = "halo";
+        
+        if (kata1.equalsIgnoreCase(kata2)){
+            System.out.println("Kata-1 sama dengan kata-2");
+        }else{
+            System.out.println("Kata-1 tidak sama dengan kata-2");
+        }
+    }
+}
