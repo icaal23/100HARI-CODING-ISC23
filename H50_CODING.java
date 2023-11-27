d91cece93ba68cdf669f901520223281fac80a9f
ical <123912919+icaal23@users.noreply.github.com>
2023-11-27 18:59:57 +0800

    Create H50_CODING.java
    Program penggunaan String.format()

---
+++ H50_CODING.java
@@ -0,0 +1,9 @@
+public class H50_CODING {
+    public static void main(String[] args) {
+        String nama = "Ical";
+        int umur = 18;
+        
+        String hasil = String.format ("Nama : %s, Umur : %d", nama, umur);
+        System.out.println(hasil);
+    }
+}
