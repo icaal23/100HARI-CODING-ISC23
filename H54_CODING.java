1c2ae1c2a4967312b10374d010ceedd8857b8417
ical <123912919+icaal23@users.noreply.github.com>
2023-12-01 21:03:35 +0800

    Create H54_CODING.java
    Program yang menggunakan Penugasan (+=)

---
+++ H54_CODING.java
@@ -0,0 +1,10 @@
+public class H54_CODING {
+    public static void main(String[] args) {
+        int angkaPertama = 60;
+        int angkaKedua = 10;
+        
+        int hasil = angkaPertama;
+        hasil += angkaKedua;
+        System.out.println("Hasil : "+ hasil);
+    }
+}
