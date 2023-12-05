2c841ae121acb72555ede728ad97f0967feb908d
ical <123912919+icaal23@users.noreply.github.com>
2023-12-05 21:31:54 +0800

    Create H57_CODING.java
    Program penggunaan break dalam loop

---
+++ H57_CODING.java
@@ -0,0 +1,11 @@
+public class H57_CODING {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                System.out.println("Loop dihentikan karena i mencapai angka 3");
+                break;
+            }
+            System.out.println("Nilai i: " + i);
+        }
+    }
+}
