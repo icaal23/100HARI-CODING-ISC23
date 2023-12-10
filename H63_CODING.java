b40aaf31add72bdb509185c7f85aa50a4d5b01ca
ical <123912919+icaal23@users.noreply.github.com>
2023-12-10 22:24:27 +0800

    Create H63_CODING.java
    Program penggunaan Logika AND (&&)

---
+++ H63_CODING.java
@@ -0,0 +1,11 @@
+public class H63_CODING {
+    public static void main(String[] args) {
+        boolean kondisi = true;
+        boolean kondisi2 = false;
+        
+        //Logika AND
+        boolean hasil = kondisi && kondisi2;
+        System.out.println("Hasilnya : "+ hasil);
+        
+    }
+}
