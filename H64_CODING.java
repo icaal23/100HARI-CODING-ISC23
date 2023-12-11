bfcc8873a3713c52a7149d84f7609f7cfc145970
ical <123912919+icaal23@users.noreply.github.com>
2023-12-11 22:49:12 +0800

    Create H64_CODING.java
    Program penggunaan Operasi Logika OR (||)

---
+++ H64_CODING.java
@@ -0,0 +1,10 @@
+
+public class H64_CODING {
+    public static void main(String[] args) {
+        boolean kondisi = true;
+        boolean kondisi2  = false;
+        
+        boolean hasil = kondisi || kondisi2;
+        System.out.println("Hasil : "+ hasil);
+    }
+}
