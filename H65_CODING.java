d58fd9754cce1a79c2674b544689c23100dfcced
ical <123912919+icaal23@users.noreply.github.com>
2023-12-12 19:03:53 +0800

    Create H65_CODING.java
    Program penggunaan Operator NOT (!)

---
+++ H65_CODING.java
@@ -0,0 +1,10 @@
+
+public class H65_CODING {
+    public static void main(String[] args) {
+        boolean kondisi = true;
+        boolean kondisi2  = false;
+        
+        boolean hasil = !kondisi;
+        System.out.println("Hasil : "+ hasil);
+    }
+}
