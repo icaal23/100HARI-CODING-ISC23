ec09a00072f77a0dd10b7aebc07bb1af5e859898
ical <123912919+icaal23@users.noreply.github.com>
2023-12-25 21:04:11 +0800

    Create H78_CODING.java
    Program penggunaan vector

---
+++ H78_CODING.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class H78_CODING {
+    public static void main(String[] args) {
+        Vector<String> a = new Vector<>();
+        a.add("Yamaha");
+        a.add("Honda");
+        a.add("Kawasaki");
+        
+        System.out.println("Hasil : ");
+        for(String produk : a){
+            System.out.println(produk);
+        }
+    }
+}
