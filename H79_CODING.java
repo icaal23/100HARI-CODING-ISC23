7b4e855f7a269f5fdc4a9a2f848dfda3fce52628
ical <123912919+icaal23@users.noreply.github.com>
2023-12-26 21:13:25 +0800

    Create H79_CODING.java
    Program penggunaan ArrayList yang tidak bertipe

---
+++ H79_CODING.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class H79_CODING {
+    public static void main(String[] args) {
+        ArrayList a = new ArrayList();
+        a.add("Ical");
+        a.add(18);
+        a.add(169.5);
+        
+        for(Object isi : a){
+            System.out.println(isi);
+        }
+    }
+}
