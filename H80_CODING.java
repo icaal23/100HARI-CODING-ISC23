b654d23de3bb73429f898bbf62e79e9efc8b598c
ical <123912919+icaal23@users.noreply.github.com>
2023-12-27 21:24:10 +0800

    Create H80_CODING.java
    Program penggunaan ArrayList bertipe Wrapper

---
+++ H80_CODING.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class H80_CODING {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        System.out.println("Hasil : "+ list);
+    }
+}
