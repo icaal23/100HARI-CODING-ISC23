47f3174a725426c42870e23bf721fd2cf4566059
ical <123912919+icaal23@users.noreply.github.com>
2023-12-28 21:09:35 +0800

    Create H81_CODING.java
    Program penggunaan vector bertipe Wrapper

---
+++ H81_CODING.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class H81_CODING {
+    public static void main(String[] args) {
+        Vector<Double> list = new Vector<>();
+        list.add(10.1);
+        list.add(20.2);
+        System.out.println("Hasil : "+ list);
+    }
+}
