82af3d67fe723fbd1a868965c8f107387f1ddf7a
ical <123912919+icaal23@users.noreply.github.com>
2023-12-29 21:30:24 +0800

    Create H82_CODING.java
    Program penggunaan Vector bertipe String

---
+++ H82_CODING.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class H82_CODING {
+    public static void main(String[] args) {
+        Vector<String> list = new Vector<>();
+        list.add("Ical");
+        list.add("El");
+        
+        System.out.println("Hasil : ");
+        for(String hasil : list){
+            System.out.println(hasil);
+        }
+    }
+}
