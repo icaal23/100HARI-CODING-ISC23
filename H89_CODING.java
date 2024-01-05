43a34ce8f35073311d6a85fee8c1fad267eabacd
ical <123912919+icaal23@users.noreply.github.com>
2024-01-05 19:59:36 +0800

    Create H89_CODING.java
    Program penggunaan offer()

---
+++ H89_CODING.java
@@ -0,0 +1,11 @@
+import java.util.Queue;
+import java.util.LinkedList;
+public class H89_CODING {
+    public static void main(String[] args){
+        Queue<Integer> list = new LinkedList<>();
+        list.offer(1);
+        list.offer(2);
+        list.offer(3);
+        System.out.println("Hasil : " + list);
+    }
+}
