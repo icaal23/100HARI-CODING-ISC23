87d6853fab6599192625ecd0ab453385532d1891
ical <123912919+icaal23@users.noreply.github.com>
2024-01-14 21:34:40 +0800

    Create H98_CODING.java
    Program penggunaan pollLast()

---
+++ H98_CODING.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class H98_CODING {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Tas");
+        list.add("Jam");
+        list.add("Buku");
+        System.out.println("Sebelum : " + list);
+        
+        String lastElement = list.pollLast();
+        System.out.println("Elemen pertama yang dihapus: " + lastElement);
+        System.out.println("Sesudah : " + list);
+    }
+    
+}
